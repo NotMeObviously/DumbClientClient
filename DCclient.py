aacf5456c0ccd7e704a44583508556361a0e8632
NotMeObviously <62179402+NotMeObviously@users.noreply.github.com>
2021-07-12 21:10:18 +0200

    Update DCclient.py

--- DCclient.py
+++ DCclient.py
@@ -1,8 +1,49 @@
+import json
 import git
+import time
+import os
+import socket
 
-print("Hello, i'm running!")
-while True:
-    id=1
+def search_server_to_connect():
+    opened = False
+    while not opened:
+        try:
+            f=open("setup\\address.txt","r")
+            text = f.read()
+            host, port = text.split(":")
+        except:
+            time.sleep(20)
+            pull()
+    return int(host),int(port)
+
+target_host, target_port = search_server_to_connect()
+hostname = os.getlogin()
+
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connected = False
+while not connected:
+    try:
+        client.connect((target_host,target_port))
+        connected = True
+    except:
+        print("errore connessione")
+    time.sleep(3)
+
+close = False
+
+msg = "Hello! i'm " + hostname + " give me commands"
+client.send(msg.encode())
+while not close:
+
+    response = (client.recv(4096)).decode()
+    response_json = json.loads(response)
+    print(response_json)
+    if response_json["op"] == "echo":
+        client.send(response_json["args"].encode())
+    elif response_json["op"] == "close":
+        client.close()
+        close = True
 
 def pull():
     repo = git.Repo('./')
